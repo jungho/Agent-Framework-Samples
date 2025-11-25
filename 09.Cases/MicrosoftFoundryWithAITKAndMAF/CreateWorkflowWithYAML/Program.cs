63b593d41357f68520a8dfc4cf24a9e22dc76851
kinfey <lokinfey@gmail.com>
2025-11-25 13:37:14 +0800

    add samples

---
+++ 09.Cases/MicrosoftFoundryWithAITKAndMAF/CreateWorkflowWithYAML/Program.cs
@@ -0,0 +1,27 @@
+﻿using Azure.Identity;
+using Microsoft.Extensions.AI;
+using Microsoft.Extensions.Configuration;
+using OpenAI.Responses;
+
+using Microsoft.Agents.AI.Workflows;
+using Microsoft.Agents.AI.Workflows.Declarative;
+using Microsoft.Extensions.AI;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+
+
+internal sealed class Program
+{
+    public static async Task Main(string[] args)
+    {
+        // IConfiguration configuration = Application.InitializeConfig();
+        Uri foundryEndpoint = new("https://kinfey-ai-ignite-demo-resource.services.ai.azure.com/api/projects/kinfey-ai-ignite-demo");
+
+        WorkflowFactory workflowFactory = new("Your workflow.yaml path", foundryEndpoint);
+        WorkflowRunner runner = new();
+        await runner.ExecuteAsync(workflowFactory.CreateWorkflow, "junior developer");    
+    }
+}
+    
+
