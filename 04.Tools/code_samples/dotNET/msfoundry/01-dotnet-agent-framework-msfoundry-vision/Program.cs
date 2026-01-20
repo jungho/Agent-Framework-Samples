9daf0111a3c67c77f799cefb7c6f9355e3d04c8c
kinfey <kinfeylo@microsoft.com>
2026-01-20 23:11:19 +0800

    Update API Change for .NET & Python

---
+++ 04.Tools/code_samples/dotNET/msfoundry/01-dotnet-agent-framework-msfoundry-vision/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using Azure.AI.Projects;
+using Azure.AI.Projects.OpenAI;
+using Azure.Identity;
+using Microsoft.Agents.AI;
+using Microsoft.Extensions.AI;
+ using DotNetEnv;
+
+ Env.Load("../../../../../.env");
+
+
+var azure_foundry_endpoint = Environment.GetEnvironmentVariable("AZURE_AI_PROJECT_ENDPOINT") ?? throw new InvalidOperationException("AZURE_AI_PROJECT_ENDPOINT is not set.");
+var azure_foundry_model_id = "gpt-4o";
+
+var imgPath ="../../../files/home.png";
+
+const string AgentName = "Vision-Agent";
+const string AgentInstructions = "You are my furniture sales consultant, you can find different furniture elements from the pictures and give me a purchase suggestion";
+
+
+async Task<byte[]> OpenImageBytesAsync(string path)
+{
+	return await File.ReadAllBytesAsync(path);
+}
+
+var imageBytes = await OpenImageBytesAsync(imgPath);
+
+AIProjectClient aiProjectClient = new(
+    new Uri(azure_foundry_endpoint),
+    new AzureCliCredential());
+
+AgentVersionCreationOptions options = new(new PromptAgentDefinition(model: azure_foundry_model_id) { Instructions = AgentInstructions });
+AgentVersion createdAgentVersion = aiProjectClient.Agents.CreateAgentVersion(agentName: AgentName, options);
+
+
+AIAgent agent = aiProjectClient.GetAIAgent(createdAgentVersion);
+
+ChatMessage userMessage = new ChatMessage(ChatRole.User, [
+	new TextContent("Can you identify the furniture items in this image and suggest which ones would fit well in a modern living room?"), new DataContent(imageBytes, "image/png")
+]);
+
+
+AgentThread thread = await agent.GetNewThreadAsync();
+
+
+Console.WriteLine(await agent.RunAsync(userMessage, thread));
+
